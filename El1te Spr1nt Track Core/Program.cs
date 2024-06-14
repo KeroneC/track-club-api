bbc445e32bbda22c43d8481751016e4569710a5e
Kerone <kerone.ant.creary@hotmail.com>
2024-06-14 10:52:04 -0400

    added cors configuration

--- El1te Spr1nt Track Core/Program.cs
+++ El1te Spr1nt Track Core/Program.cs
@@ -7,6 +7,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +31,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// Use CORS with the specified policy
+app.UseCors("AllowSpecificOrigins");
 
 app.MapControllers();
 
